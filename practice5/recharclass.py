e1c1ed2cadbcf0d1d0d2a8b848437cd0fa4d3ba6
kamilasadvakasova <k_sadvakasova@kbtu.kz>
2026-03-02 20:11:00 +0500

    Create recharclass.py

---
+++ practice5/recharclass.py
@@ -0,0 +1,29 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has any a, r, or n characters:
+
+x = re.findall("[arn]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+----------------
+import re
+
+txt = "8 times before 11:45 AM"
+
+#Check if the string has any characters from a to z lower case, and A to Z upper case:
+
+x = re.findall("[a-zA-Z]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
