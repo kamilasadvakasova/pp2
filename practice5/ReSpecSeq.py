623eb614548e07bed3d947da96dd5245db62733c
kamilasadvakasova <k_sadvakasova@kbtu.kz>
2026-03-02 20:10:03 +0500

    Create ReSpecSeq.py

---
+++ practice5/ReSpecSeq.py
@@ -0,0 +1,45 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string starts with "The":
+
+x = re.findall("\AThe", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is a match!")
+else:
+  print("No match")
+----
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string ends with "Spain":
+
+x = re.findall("Spain\Z", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is a match!")
+else:
+  print("No match")
+
+---
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string contains any digits (numbers from 0-9):
+
+x = re.findall("\d", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
